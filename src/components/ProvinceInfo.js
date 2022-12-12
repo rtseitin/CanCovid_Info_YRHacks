17cdc7ce3005adb6e636735edacc1f3087cdd997
thinkpuppy <341181484@gapps.yrdsb.ca>
2022-12-12 17:42:20 -0500

    update code to reflect api changes

--- src/components/ProvinceInfo.js
+++ src/components/ProvinceInfo.js
@@ -3,33 +3,19 @@ import { useEffect, useState } from 'react';
 const DetailedProvinceInfo = ({ province }) => {
   const [activeCases, setActiveCases] = useState(null);
   const [cumulativeDeaths, setCumulativeDeaths] = useState(null);
-  const [recoveredCases, setRecoveredCases] = useState(null);
+  const [totalHospitalizations, setHospitalizations] = useState(null);
 
   useEffect(() => {
-    const yesterdayDate = new Date(new Date().toLocaleString("en-US", { timeZone: "America/Toronto" })).getTime() - 1000 * 60 * 60 * 24;
-    const formattedDate = new Date(yesterdayDate).toLocaleDateString('pt-br').split( '/' ).reverse( ).join( '-' )
-    fetch(`https://api.opencovid.ca/timeseries?loc=${province.code}&after=${formattedDate}`)
+    fetch(`https://api.opencovid.ca/timeseries?loc=${province.code}`)
       .then(res => {
         return res.json()
       })
       .then(data => {
-        if (data.active[0].active_cases === 0) {
-          setActiveCases(null)
-        } else {
-          setActiveCases(data.active[0].active_cases)
-        }
-
-        if (data.mortality[0].cumulative_deaths === 0) {
-          setCumulativeDeaths(null)
-        } else {
-          setCumulativeDeaths(data.mortality[0].cumulative_deaths)
-        }
-
-        if (data.mortality[0].cumulative_recovered === 0) {
-          setRecoveredCases(null)
-        } else {
-          setRecoveredCases(data.recovered[0].cumulative_recovered)
-        }
+      data = data.data
+        setActiveCases(data.cases[data.cases.length-1].value)
+        setCumulativeDeaths(data.deaths[data.deaths.length-1].value)
+        setHospitalizations(data.hospitalizations[data.hospitalizations.length-1].value)
+        console.log(totalHospitalizations)
       });
   }, [province.code]);
 
@@ -39,19 +25,19 @@ const DetailedProvinceInfo = ({ province }) => {
 
       <div className="grid">
         <div className="card">
-          {activeCases && <h1>{activeCases.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}</h1>}
-          {!activeCases && <h1>0</h1>}
+          {activeCases != null&& <h1>{activeCases.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}</h1>}
+          {activeCases == null && <h1>Loading...</h1>}
           <h3 style={{color: "white"}}>Total Cases</h3>
         </div>
         <div className="card">
-          {cumulativeDeaths && <h1>{cumulativeDeaths.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}</h1>}
-          {!cumulativeDeaths && <h1>0</h1>}
+          {cumulativeDeaths != null && <h1>{cumulativeDeaths.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}</h1>}
+          {cumulativeDeaths == null && <h1>Loading..</h1>}
           <h3 style={{color: "white"}}>Total Deaths</h3>
         </div>
         <div className="card">
-          {recoveredCases && <h1>{recoveredCases.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}</h1>}
-          {!recoveredCases && <h1>0</h1>}
-          <h3 style={{color: "white"}}>Recovered Cases</h3>
+          {totalHospitalizations != null && <h1>{totalHospitalizations.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}</h1>}
+          {totalHospitalizations == null && <h1>Loading...</h1>}
+          <h3 style={{color: "white"}}>Hospitalizations</h3>
         </div>
       </div>
       
